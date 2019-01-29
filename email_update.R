ff3f4e7e550f6cfb38fd3e8578855f3c4e65fd0b
Jay Winiarski <jasonwiniarski@u.boisestate.edu>
2019-01-29 10:28:23 -0700

    Update email_update.R

--- email_update.R
+++ email_update.R
@@ -5,13 +5,15 @@ time_in_seconds <- 10*60
 
 Sys.sleep(time_in_seconds) # add delay here to allow time to upload the new map to my github site
 
+ccList <- c("anjolenehunt@boisestate.edu", 
+            "jessewatson@boisestate.edu", 
+            "fcphenology@boisestate.edu")
+
 ## let others know that the new map is ready to view
-msg <- mime() %>%
+msg = mime() %>%
   from("jasonwiniarski@u.boisestate.edu") %>%
   to("julieheath@boisestate.edu") %>%
-  cc(c("anjolenehunt@boisestate.edu", 
-       "jessewatson@boisestate.edu", 
-       "fcphenology@boisestate.edu")) %>%
+  cc(ccList) %>%
   subject(paste0("New kestrel tracks - ", Sys.Date())) %>%
   text_body("Hi,
 
