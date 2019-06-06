3f70e66f84fb4c1c27e061b79334768638f73ae1
jaymwin <jasonwiniarski@u.boisestate.edussh-keygen -t rsa -C jasonwiniarski@u.boisestate.edu>
2019-06-06 12:07:35 -0600

    changed update_map script

--- update_map.R
+++ update_map.R
@@ -1,8 +1,13 @@
 
 library(here)
+library(purrr)
 
 here <- here::here
 
-source('combine_csv_files.R')
-source('mapview.R')
+#source('combine_csv_files.R')
+#source('mapview.R')
 #source('email_update.R')
+
+files <- c('combine_csv_files.R', 'mapview.R')
+
+map(files, source)
