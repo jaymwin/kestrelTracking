9b76460eeca2d3ce771b67a4e21a172b1d9702db
jaymwin <jasonwiniarski@u.boisestate.edussh-keygen -t rsa -C jasonwiniarski@u.boisestate.edu>
2019-07-12 10:29:32 -0600

    renamed scripts

--- 05-update_map.R
+++ 05-update_map.R
@@ -2,7 +2,7 @@
 library(here)
 library(tidyverse)
 
-files <- c('combine_csv_files.R', 'mapview.R', 'plot_timeline.R')
+files <- c('01-combine_csv_files.R', '02-mapview.R', '04-plot_timeline.R')
 
 # run scripts
 files %>%
